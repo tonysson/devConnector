c2bef8ef68b33a3bfbc18157f890a4278374dcb2
TEYI <tonysson7@gmail.com>
2020-06-18 03:43:19 +0200

    finish app

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const userRoute = require('./routes/api/userRoutes');
 const profileRoute = require('./routes/api/profileRoutes');
 const authRoute = require('./routes/api/authRoutes');
 const postsRoute = require('./routes/api/postsRoutes');
+const path = require('path');
 
 // initialize express
 const app = express();
@@ -21,6 +22,15 @@ app.use('/api/auth', authRoute);
 app.use('/api/profile', profileRoute);
 app.use('/api/posts', postsRoute);
 
+//Serve static assets in production
+if(process.env.NODE.ENV === 'production'){
+    //set Static folder
+    app.use(express.static('client/build'));
+
+    app.get('*' , (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client' , 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
